234f914346b171f1cecf90230caa7262e812aff1
伊藤京子 <okano@u-d.jp>
2020-02-20 19:00:48 +0900

    カートページ単価消す

--- wc_templates/cart/wc_cart_page.php
+++ wc_templates/cart/wc_cart_page.php
@@ -3,6 +3,12 @@
 get_header();
 ?>
 
+<style>
+.aright.unitprice {
+	display:none;
+}
+	</style>
+
 <div id="content" class="two-column">
 <div class="catbox">
 
@@ -47,12 +53,12 @@ get_header();
 						<tr>
 							<th scope="row" class="num">No.</th>
 							<!-- <th class="thumbnail"> </th> -->
-							<th><?php _e('item name', 'usces'); ?></th>
-							<th class="quantity"><?php _e('Unit price', 'usces'); ?></th>
+							<th class="item__th"><?php _e('item name', 'usces'); ?></th>
+						<!-- 	<th class="quantity"><?php _e('Unit price', 'usces'); ?></th> -->
 							<!-- <th class="quantity"><?php _e('Quantity', 'usces'); ?></th> -->
 							<th class="subtotal"><?php _e('Amount', 'usces'); ?><?php usces_guid_tax(); ?></th>
 						<!-- 	<th class="stock"><?php _e('stock status', 'usces'); ?></th> -->
-							<th class="action" colspan="2">&nbsp;</th>
+							<th class="action">&nbsp;</th>
 						</tr>
 						</thead>
 						<tbody>
@@ -60,7 +66,13 @@ get_header();
 						</tbody>
 						<tfoot>
 						<tr>
-							<th colspan="4" scope="row" class="aright"><?php _e('total items', 'usces'); ?><?php usces_guid_tax(); ?></th>
+<?php if (wp_is_mobile()) : ?>
+	<th colspan="2" scope="row" class="aright"><?php _e('total items', 'usces'); ?><?php usces_guid_tax(); ?></th>
+<?php else: ?>
+	<th colspan="2" scope="row" class="aright"><?php _e('total items', 'usces'); ?><?php usces_guid_tax(); ?></th>
+<?php endif; ?>
+
+
 							<th class="tc price__txt" colspan="2"><?php usces_crform(usces_total_price('return'), true, false); ?></th>
 						<!-- 	<th colspan="2">&nbsp;</th> -->
 						</tr>
